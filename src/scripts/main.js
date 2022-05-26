ca199a4ce333fe577310e1368f5eb80535d2c0a3
Nell Maissin <nellmaissin@gmail.com>
2022-05-26 19:12:06 +0200

    Added js for horizontal scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
+import gsap from "../../node_modules/gsap";
+import ScrollTrigger from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
 // Date copyright
 
 let date = new Date();
@@ -39,4 +43,20 @@ function dark(){
 function light(){
     document.body.setAttribute("data-theme", "light");
     ball.classList.remove("btn--ballDark");
+}
+
+
+// Greensock
+if(window.matchMedia('(min-width: 768px)').matches){
+    gsap.to(".horizontal", {
+        x: '-200vw',
+        scrollTrigger: {
+            trigger: '.horizontal',
+            pin: true,
+            start: 'center center',
+            end: '+=5000',
+            scrub: true,
+            toggleActions: 'play none reverse none'
+        }
+    });
 }
